38e1ff2bf9920e7073cafcc035daac2874c23c8d
zzzaitegui <tzj077@inlumine.ual.es>
2025-11-07 02:53:30 +0100

    Ported client side version to NeoForge 1.20.1

--- src/main/java/net/memeland/bsf/BetterSnowyFeaturesMod.java
+++ src/main/java/net/memeland/bsf/BetterSnowyFeaturesMod.java
@@ -1,12 +1,7 @@
 package net.memeland.bsf;
 
-import net.memeland.bsf.block.ModBlocks;
 import net.memeland.bsf.client.model.SnowOverlayBakedModel;
-import net.memeland.bsf.item.ModCreativeModeTab;
-import net.memeland.bsf.item.ModItems;
 import net.memeland.bsf.util.BiomeTemperatureHelper;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -17,22 +12,16 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Mod(BetterSnowyFeaturesMod.MOD_ID)
 public class BetterSnowyFeaturesMod {
 
     public static final String MOD_ID = "bsf";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public BetterSnowyFeaturesMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModConfig.register();
-        ModCreativeModeTab.CREATIVE_MODE_TABS.register(modEventBus);
-        ModBlocks.register(modEventBus);
-        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
@@ -49,7 +38,6 @@ public class BetterSnowyFeaturesMod {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
             event.enqueueWork(() -> {
-                ItemBlockRenderTypes.setRenderLayer(ModBlocks.ICICLE.get(), RenderType.translucent());
             });
         }
     }
